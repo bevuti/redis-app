739034031bd92996c9761280536b3e882970ab97
bevuti <bevuti@gmail.com>
2022-04-25 13:08:09 -0300

    Update teste-app.sh

--- teste-app.sh
+++ teste-app.sh
@@ -1,1 +1,14 @@
+#bin/bash
 #teste-app.sh
+RESULT="'wget -qO- http://localhost:8090'"
+wget -q localhost:8090
+if [$? -eq 0]
+then
+    echo 'OK - serviço no ar!'
+elif[[$RESULT == *"Number"*]]
+then
+    echo 'OK - serviço no ar!'
+    echo $RESULT
+else
+    echo "NAO DEU"
+    exit 1
